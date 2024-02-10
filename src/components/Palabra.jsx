f1503963d0fede02b42822b4dc7ad9a29ba166a0
Sergionx <sergio180203@gmail.com>
2024-02-09 23:45:41 -0400

    Refactor Palabra component SVG attributes

--- src/components/Palabra.jsx
+++ src/components/Palabra.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
+import React from "react";
 import { motion } from "framer-motion";
 
 const propuestaVariants = {
   hidden: {
     pathLength: 0,
-    fill:"rgba(0, 0 ,0, 0)"
+    fill: "rgba(0, 0 ,0, 0)",
   },
   visible: {
     pathLength: 1,
-    fill:"rgb(205, 60, 60)"
+    fill: "rgb(205, 60, 60)",
   },
 };
 
-export default function Palabra({ path }) {
+export default function Palabra({ viewBox, width, path }) {
   return (
     <motion.svg
-        viewBox="0 0 114 48"
-        xmlns="http://www.w3.org/2000/svg"
-        className="stroke-1 stroke-black  w-full"
-      >
-         <motion.path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          variants={propuestaVariants}
-          initial="hidden"
-          animate="visible"
-          transition={{
-            default: { duration: 5, ease: "easeInOut" },
-            fill: { duration: 5, ease: [1, 0, 0.8, 1] },
-          }} 
-          d={path}
-          />
+      viewBox={viewBox}
+      width={width}
+      height="100pt"
+      xmlns="http://www.w3.org/2000/svg"
+      className="stroke-1 stroke-[#561b1b] h-64 "
+    >
+      <motion.path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        variants={propuestaVariants}
+
+        transition={{
+          default: { duration: 4, ease: "easeInOut" },
+          fill: { duration: 2, ease: [1, 0.2, 1, 1] },
+        }}
+        d={path}
+      />
     </motion.svg>
-  )
+  );
 }
