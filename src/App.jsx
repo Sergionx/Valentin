27952aed78fe388f0b01bd2cbbddad305ca4ae67
Sergionx <sergio180203@gmail.com>
2024-02-09 23:54:00 -0400

    Fondo gradiente

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Propuesta from './components/Propuesta'
 
+// TODO - Reproducir musica 
+// TODO - Botones para aceptar
 function App() {
 
   return (
     <>
+    <div className='from-white to-[#f69ed1] bg-gradient-radial min-h-screen'>
+
       <Propuesta />
+    </div>
     </>
   )
 }
