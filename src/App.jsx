6b3ce2cc8d6f9164cd39ac2c32cfac949c14b823
Sergionx <sergio180203@gmail.com>
2024-02-10 00:08:58 -0400

    Confetti de corazones preliminar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Propuesta from './components/Propuesta'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Propuesta from "./components/Propuesta";
+import JSConfetti from "js-confetti";
 
-// TODO - Reproducir musica 
+// TODO - Reproducir musica
 // TODO - Botones para aceptar
+// TODO - Corazón pulsante
 function App() {
+  const jsConfetti = new JSConfetti();
 
+  jsConfetti.addConfetti(
+    {
+      emojis: ["❤️", "💕", "💖", "💞", "💘", "💝"],
+      
+    },
+  );
   return (
     <>
-    <div className='from-white to-[#f69ed1] bg-gradient-radial min-h-screen'>
-
-      <Propuesta />
-    </div>
+      <div className="from-white to-[#f69ed1] bg-gradient-radial min-h-screen">
+        <Propuesta />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
