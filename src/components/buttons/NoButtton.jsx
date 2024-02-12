e4ad01bf1d28d418724df2cc9abaea81128ea870
Sergionx <sergio180203@gmail.com>
2024-02-12 01:15:02 -0400

    Add shake animation to NoButton component

--- src/components/buttons/NoButtton.jsx
+++ src/components/buttons/NoButtton.jsx
@@ -1,21 +1,29 @@
-import { motion } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
 import { useMotionValue } from "framer-motion";
-// TODO - Considerar poner shake when click
+
 export default function NoButtton() {
+  const [scope, animate] = useAnimate();
+  const shakeX = useMotionValue(0);
+
   const scale = useMotionValue(1);
   const handleClick = () => {
+    animate(shakeX, [-20, 20, -20, 20, 0], {
+      type: 'spring',
+      stiffness: 1000,
+      damping: 20,
+    });
     scale.set(scale.get() - 0.1);
-    console.log(scale.get());
   };
+
   return (
     <motion.button
+      ref={scope}
       className="border-2 border-[#da479c]
     rounded-full px-8 py-4 font-bold
     text-[#da479c]
     "
-      style={{ scale }}
-    
-      transition={{ type: "spring", stiffness: 400 }}
+    style={{ scale, x: shakeX }}
+    transition={{ type: "spring", stiffness: 400 }}
       onClick={handleClick}
     >
       No
