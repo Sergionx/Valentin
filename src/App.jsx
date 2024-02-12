dcefe0853fe5e823aee850e5c03ba35bad76dff9
Sergionx <sergio180203@gmail.com>
2024-02-12 01:36:04 -0400

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import Propuesta from "./components/Propuesta";
-import JSConfetti from "js-confetti";
 import Decision from "./components/Decision";
 
-// TODO - Reproducir musica
 // TODO - Corazón pulsante
 function App() {
-  const jsConfetti = new JSConfetti();
-
-  jsConfetti.addConfetti({
-    emojis: ["❤️", "💕", "💖", "💞", "💘", "💝"],
-  });
+  
   return (
     <>
       <div
-        className="from-white to-[#f69ed1] bg-grdient-radial min-h-screen
+        className="from-white to-[#f69ed1] bg-gradient-radial min-h-screen
           font-valentine"
       >
         <Propuesta />
