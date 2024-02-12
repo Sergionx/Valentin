488cc5eae19f245a2cf2f0e506ef3a4d0e68988d
Sergionx <sergio180203@gmail.com>
2024-02-12 01:35:50 -0400

    Add audio to Decision component

--- src/components/Decision.jsx
+++ src/components/Decision.jsx
@@ -1,15 +1,23 @@
+import { useRef } from "react";
 import CoolButton from "./buttons/CoolButton";
 import NoButtton from "./buttons/NoButtton";
 import JSConfetti from "js-confetti";
 
+// TODO - Mostrar un mensaje de te amo en la pantalla
 export default function Decision() {
+  const audio = useRef(new Audio('/Ferris_Wheel.mp3'));
 
-  function onAccept(){
+  function onAccept() {
     const jsConfetti = new JSConfetti();
 
-  jsConfetti.addConfetti({
-    emojis: ["❤️", "💕", "💖", "💞", "💘", "💝"],
-  });
+    jsConfetti.addConfetti({
+      emojis: ["❤️", "💕", "💖", "💞", "💘", "💝"],
+    });
+
+    audio.current.volume = 0.5;
+    audio.current.pause();
+    audio.current.currentTime = 0;
+    audio.current.play();
   }
 
   return (
